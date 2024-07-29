d8f9bad4a778b7fd2045bbf58a4cafa339739538
Matthew Boughton <mboughton17@gmail.com>
2024-07-29 17:54:34 -0400

    Artist Overall Popularity Ranking Fixes #2 began working on classes for popularity ranking artists and songs

---
+++ NicheRank/playlist_management/metrics_handing.py
@@ -0,0 +1,26 @@
+
+"""
+    Organize different methods of taking songs and running metrics on them
+    
+"""
+
+
+from typing import Any
+
+
+class Song_Popularity_Ranking():
+
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass
+
+
+class Artist_Metric_Ranking():
+
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass
