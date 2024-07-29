cd243a1790e64acf42b50e6c4f3182de6cbdf309
Matthew Boughton <mboughton17@gmail.com>
2024-07-29 17:51:20 -0400

    Playlist Database Interaction Class Fixes #5, worked to add my own python scripting to database

---
+++ NicheRank/playlist_management/metrics_handing.py
@@ -0,0 +1,16 @@
+
+"""
+    Organize different methods of taking songs and running metrics on them
+"""
+
+
+from typing import Any
+
+
+class Song_Popularity_Ranking():
+
+    def __init__(self) -> None:
+        pass
+
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        
