5bfa0ab1f2c976c92139b283fc7964fa5962e26d
Matthew Boughton <mboughton17@gmail.com>
2024-08-02 18:41:32 -0400

    handling for load percent

--- playlist_parse.py
+++ playlist_parse.py
@@ -8,17 +8,24 @@ from NicheRank.algo_src.file_management import Dataset_Extractor
     This script is for Generating Your Own database from the Playlist Dataset. 
 """
 
-
+DEFAULT_LP = 0.1
 
 parser = argparse.ArgumentParser(description="A simple example of argparse")
 
-parser.add_argument("--load_percent", default=0.1, help="Percent of 1 Million Playlists to load")
+parser.add_argument("--load_percent", default=DEFAULT_LP, help="Percent of 1 Million Playlists to load")
 parser.add_argument("--profile", default=True, help="Display progress or not")
+
+def handle_lp(args) -> float:
+    lf = float(args.load_percent)
+    if (lf > 1.0 or lf< 0.0):
+        lf = DEFAULT_LP
+
+    return lf
 
 def main():
     args = parser.parse_args()
-    lp = args.load_percent
-    playlist_dir = "NicheRank/playlist_database"
+    lp = handle_lp(args)
+    playlist_dir = "NicheRank/playlist_dataset"
     playist_path = os.path.join(playlist_dir, "spotify_million_playlist_dataset")
     extractor = Dataset_Extractor(playist_path, profile=args.profile)
     extractor.create_database(load_percent=lp, save=True)
