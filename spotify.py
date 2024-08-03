5b49fe1b8526e75940e3c8e99953f513dd138fd2
Matthew Boughton <55923494+mattcattb@users.noreply.github.com>
2024-08-03 17:25:24 -0400

    testing changes

--- spotify.py
+++ spotify.py
@@ -3,6 +3,8 @@ from flask import Flask, request, redirect, session, url_for
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.cache_handler import FlaskSessionCacheHandler
+import NicheRank.algo_src.control as ctrl 
+from NicheRank.algo_src.analyze_history import User_Metrics
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
@@ -51,8 +53,8 @@ def get_recently_played():
     #song_names_html = '<br>'.join([f'{name}: <a href="{url}">{url}</a>' if url else name for name, url in song_uri])
 
     #gets only uris in a list, not formatted
-    song_uris = [item['track']['uri'] for item in data.get('items', [])]
-    
+    sorting_type = "q" # can be q or m
+    metrics: User_Metrics = ctrl.get_metrics_spotify_user(data, database_name=ctrl.DEFAULT_DATABASE, sorting_type=sorting_type)
     return song_uris
 
 @app.route('/logout')
