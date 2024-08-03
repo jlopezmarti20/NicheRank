cd7bac27d67a6a32b5977240c6f4c59159a05a69
Matthew Boughton <55923494+mattcattb@users.noreply.github.com>
2024-08-03 16:29:12 -0400

    Update users.py

--- NicheRank/algo_src/users.py
+++ NicheRank/algo_src/users.py
@@ -13,7 +13,7 @@ import music_dataclass as md
 """
 
 class UserManager():
-
+    #WAHOOOo
     def __init__(self, database=None) -> None:
         # database is what music database we are pulling from
 
