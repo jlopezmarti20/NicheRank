17e64573f6b8f656a26a278ac9e409b6642108aa
AmandaBrannon <abrannon125@gmail.com>
2024-08-03 23:11:22 -0400

    Fixed comments within spotify.py
    took out previous versions of the code, added comments for explanations of the routes and data extraction

--- spotify.py
+++ spotify.py
@@ -4,21 +4,23 @@ from flask import Flask, request, redirect, session, url_for
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 from spotipy.cache_handler import FlaskSessionCacheHandler
-import sys
 
-sys.path.append("NicheRank/algo_src")
 
-import NicheRank.algo_src.control as ctrl 
-from NicheRank.algo_src.analyze_history import User_Metrics
-
+#this is how to start the file with Flask, then create a randomized secret key
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(64)
 
+
+#this is connecting the Spotify for Developers (SfD) with my project- the client ID and Secret are what connects them
 client_id = '52500f70b3534d0bae16a8efac5a70af'
 client_secret = '42de3627a2d14129a605b2472cefbfc3'
+#this redirect uri is authorized within the SfD, and is used within the auth_url (very important!!)
 redirect_uri = 'http://localhost:5000/callback'
+#this is what determines what displays when I am asking for user permission for their data
 scope = 'user-read-recently-played'
 
+
+#this makes a new session upon opening the page- this is important as the authorization token is only temporary
 cache_handler = FlaskSessionCacheHandler(session)
 sp_oauth = SpotifyOAuth(
     client_id=client_id,
@@ -30,6 +32,11 @@ sp_oauth = SpotifyOAuth(
 )
 sp = Spotify(auth_manager=sp_oauth)
 
+
+#this is the landing page. it checks whether you have logged in yet. if you have (you probably haven't) it goes straight to collecting
+#data and automatically rerouting you to your score page. It HAS to redirect to auth_url, or else you will get stuck in a deadlock.
+#that auth_url contains the client id and redirect_uri and looks like 
+#this: https://accounts.spotify.com/authorize?client_id=52500f70b3534d0bae16a8efac5a70af&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fcallback&scope=user-read-recently-played&show_dialog=True
 @app.route('/')
 def home():
     if not sp_oauth.validate_token(cache_handler.get_cached_token()):
@@ -37,27 +44,25 @@ def home():
         return redirect(auth_url)
     return redirect(url_for('get_recently_played'))
 
+
+#this callback function happens when you have logged in- it authorizes with your access token and reroutes you to collecting data
+#and going to the score page
 @app.route('/callback')
 def callback():
     sp_oauth.get_access_token(request.args['code'])
     return redirect(url_for('get_recently_played'))
 
+
+#this happens so fast you never see the page! this checks again to make sure you are authorized. It then takes the 50 most recently
+#played songs and puts them in a .json file to be taken in by another file for analysis. the redirectUri is manually changed
+#so that you can get to your score page (this redirect connection point took us hours to figure out.)
 @app.route('/get_recently_played')
 def get_recently_played():
     if not sp_oauth.validate_token(cache_handler.get_cached_token()):
         auth_url = sp_oauth.get_authorize_url()
         return redirect(auth_url)
-    
+
     data = sp.current_user_recently_played()
-    #gets song names with url
-    #song_names_info = [(item['track']['name'], item['track'].get('external_urls', {}).get('spotify', '')) for item in data.get('items', [])]
-    #song_names_html = '<br>'.join([f'{name}: <a href="{url}">{url}</a>' if url else name for name, url in song_names_info])
-
-    #gets uris with urls
-    #song_uri = [(item['track']['uri'], item['track'].get('external_urls', {}).get('spotify', '')) for item in data.get('items', [])]
-    #song_names_html = '<br>'.join([f'{name}: <a href="{url}">{url}</a>' if url else name for name, url in song_uri])
-
-    #gets only uris in a list, not formatted
     
     file_path = "user_history.json"
     with open(file_path, "w") as f:
@@ -66,170 +71,21 @@ def get_recently_played():
     redirect_uri = 'http://127.0.0.1:8000/Score'
     return redirect(redirect_uri)
 
+"""
+@app.route('/user-metrics', methods=['GET'])
+def get_user_metrics():
+    #Example list of songs
+    songs = ["Song1", "Song2", "Song3"]
+    return jsonify(songs)
+"""
+
+#this never happens since we redirect to the frontend :)
 @app.route('/logout')
 def logout():
     session.clear()
     return redirect(url)
 
+
+#the backbone of all python files
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-"""
-client_id = '52500f70b3534d0bae16a8efac5a70af'
-client_secret = '42de3627a2d14129a605b2472cefbfc3'
-redirect_uri = 'http://localhost:5000/callback' # change this to website url
-scope = 'user-read-recently-played'
-
-cache_handler = FlaskSessionCacheHandler(session)
-sp_oauth = SpotifyOAuth(
-    client_id=client_id,
-    client_secret=client_secret,
-    redirect_uri=redirect_uri,
-    scope=scope,
-    cache_handler=cache_handler,
-    show_dialog=True
-)
-sp = Spotify(auth_manager=sp_oauth)
-
-@app.route('/')
-def home():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
-    return redirect(url_for('get_recently_played'))
-
-@app.route('/callback')
-def callback():
-    sp_oauth.get_access_token(request.args['code'])
-    print("in the /callback")
-    return redirect(url_for('get_recently_played'))
-
-@app.route('/get_recently_played')
-def get_recently_played():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
-        auth_url = sp_oauth.get_authorize_url()
-        print('hello')
-        return redirect(auth_url)
-    
-    data = sp.current_user_recently_played()
-    #gets song names with url
-    #song_names_info = [(item['track']['name'], item['track'].get('external_urls', {}).get('spotify', '')) for item in data.get('items', [])]
-    #song_names_html = '<br>'.join([f'{name}: <a href="{url}">{url}</a>' if url else name for name, url in song_names_info])
-
-    #gets uris with urls
-    #song_uri = [(item['track']['uri'], item['track'].get('external_urls', {}).get('spotify', '')) for item in data.get('items', [])]
-    #song_names_html = '<br>'.join([f'{name}: <a href="{url}">{url}</a>' if url else name for name, url in song_uri])
-
-    #gets only uris in a list, not formatted
-    song_uris = [item['track']['uri'] for item in data.get('items', [])]
-    print(song_uris)
-
-    return song_uris
-    
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect('/') # redirect to home page
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
-
-"""
-THIS IS A PREVIOUS COPY - NEED SO THAT I CAN RETURN THE SONG URIS
-import os
-from flask import Flask, request, redirect, session, url_for
-
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyOAuth
-from spotipy.cache_handler import FlaskSessionCacheHandler
-
-# https://developer.spotify.com/documentation/web-api/concepts/quota-modes
-# add user to dev board if you want to use their account (hopefully this works)
-
-# https://www.youtube.com/watch?v=2if5xSaZJlg&list=PL1TBkFFBtagorhLzvm5dCA1cOqJKxnWNz
-# https://spotipy.readthedocs.io/en/2.16.1/
-# https://developer.spotify.com/documentation/web-api/concepts/apps
-# https://stackoverflow.com/questions/67264163/spotipy-exception-raised-status-403-reason-none
-# https://community.spotify.com/t5/Spotify-for-Developers/Insufficient-Client-Scope-after-Refresh-Token/td-p/5526728 incorrect scope
-# https://developer.spotify.com/documentation/web-api/concepts/scopes the scopes
-
-# debugging errors
-
-# to run this, go to terminal, run ".\venv\Scripts\activate" then "python main.py"
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(64)
-
-client_id = '52500f70b3534d0bae16a8efac5a70af'
-client_secret = '42de3627a2d14129a605b2472cefbfc3'
-redirect_uri = 'http://localhost:5000/callback'
-scope = 'user-read-recently-played'
-
-cache_handler = FlaskSessionCacheHandler(session)
-sp_oauth = SpotifyOAuth(
-    client_id=client_id,
-    client_secret=client_secret,
-    redirect_uri=redirect_uri,
-    scope=scope,
-    cache_handler=cache_handler,
-    show_dialog=True
-)
-sp = Spotify(auth_manager=sp_oauth)
-
-@app.route('/')
-def home():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
-    return redirect(url_for('get_recently_played'))
-
-@app.route('/callback')
-def callback():
-    sp_oauth.get_access_token(request.args['code'])
-    return redirect(url_for('get_recently_played'))
-
-
-@app.route('/get_playlists')
-def get_playlists():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
-    
-    playlists = sp.current_user_playlists()
-    playlists_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlists['items']]
-    playlists_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
-
-    return playlists_html
-
-@app.route('/get_recently_played')
-def get_recently_played():
-    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
-        auth_url = sp_oauth.get_authorize_url()
-        return redirect(auth_url)
-    
-    data = sp.current_user_recently_played()
-    #gets song names with url
-    #song_names_info = [(item['track']['name'], item['track'].get('external_urls', {}).get('spotify', '')) for item in data.get('items', [])]
-    #song_names_html = '<br>'.join([f'{name}: <a href="{url}">{url}</a>' if url else name for name, url in song_names_info])
-
-    #gets uris with urls
-    #song_uri = [(item['track']['uri'], item['track'].get('external_urls', {}).get('spotify', '')) for item in data.get('items', [])]
-    #song_names_html = '<br>'.join([f'{name}: <a href="{url}">{url}</a>' if url else name for name, url in song_uri])
-
-    #gets only uris in a list, not formatted
-    song_uris = [item['track']['uri'] for item in data.get('items', [])]
-    
-    return song_uris
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-"""
